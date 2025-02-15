3ddb5a7a560a9542266b946db1943710fb63e07a
rohitlavate97 <rohitlavate97@gmail.com>
2025-02-15 14:05:40 +0530

    made changes in PaymentAPI

--- booking-service/src/main/java/com/movie/ticket/booking/system/service/controller/BookingAPI.java
+++ booking-service/src/main/java/com/movie/ticket/booking/system/service/controller/BookingAPI.java
@@ -5,10 +5,7 @@ import com.movie.ticket.booking.system.service.broker.PaymentServiceBroker;
 import com.movie.ticket.booking.system.service.dto.BookingDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookings")
@@ -24,7 +21,7 @@ public class BookingAPI {
     /*
     REST Principle: method name has to be action
     */
-    @GetMapping("/test")
+    @PostMapping
     public BookingDTO createBooking(@RequestBody BookingDTO bookingDTO) {
         log.info("Entered {} class", BookingAPI.class);
         return this.bookingService.createBooking(bookingDTO);
